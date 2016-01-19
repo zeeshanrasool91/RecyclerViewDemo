442211c20d0aa8fb39acf9ec315eff6943674d80
Zeeshan rasool <m.zeeshan.rasool@gmail.com>
2016-01-19 12:59:22 +0500

    Just A test change okay man it is new

--- app/src/main/java/com/xmy/recylerviewdemo/listener/MyItemClickListener.java
+++ app/src/main/java/com/xmy/recylerviewdemo/listener/MyItemClickListener.java
@@ -3,5 +3,7 @@ package com.xmy.recylerviewdemo.listener;
 import android.view.View;
 
 public interface MyItemClickListener {
+
+	//this is my listener
 	public void onItemClick(View view, int postion);
 }
